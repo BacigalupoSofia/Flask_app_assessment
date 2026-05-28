8a84c43fa0f1a623a341bd1d6cfd6f5aafc49911
BacigalupoSofia <bacigalupo.ie@gmail.com>
2026-05-28 22:15:27 +0100

    Add data base of dental products

---
+++ data_base.py
@@ -0,0 +1,52 @@
+products = [
+    {
+        "id": 1,
+        "category": "Protection and consumables",
+        "name": "Latex Powder Free Gloves Small Pack 100",
+        "brand": "DMI",
+        "supplier": "DMI",
+        "quantity": 3,
+        "minimum_stock": 2,
+        "expiry_date": "31/12/2027"
+    },
+    {
+        "id": 2,
+        "category": "Restorative",
+        "name": "Optibond FL primer 8ml",
+        "brand": "Kerr",
+        "supplier": "Dontalia",
+        "quantity": 2,
+        "minimum_stock": 1,
+        "expiry_date": "31/12/2027"
+    },
+    {
+        "id": 3,
+        "category": "Surgery",
+        "name": "Gelatamp Jar 50 units",
+        "brand": "Roeko",
+        "supplier": "Dontalia",
+        "quantity": 1,
+        "minimum_stock": 1,
+        "expiry_date": "31/12/2027"
+    },
+    {
+        "id": 4,
+        "category": "Restorative",  
+        "name": "Venus Pearl Syringe A1 3g",
+        "brand": "Kulzer",
+        "supplier": "Dontalia",
+        "quantity": 1,
+        "minimum_stock": 1,
+        "expiry_date": "31/12/2027"
+    },
+    {
+        "id": 5,
+        "category": "Restorative",  
+        "name": "Matrix bands Palodent standard 100 units",
+        "brand": "3M",
+        "supplier": "Henry Schein",
+        "quantity": 1,
+        "minimum_stock": 1, 
+        "expiry_date": "31/12/2027"
+    }
+]
