e37e6a043b7a0dfbface75483450216968f7c697
BacigalupoSofia <bacigalupo.ie@gmail.com>
2026-05-28 23:32:37 +0100

    python function for adding a product to products list after submiting the form

--- app.py
+++ app.py
@@ -15,7 +15,18 @@ def Stock():
 @app.route('/add', methods=['GET', 'POST'])
 def Add():
     if request.method == 'POST':
-        pass
+        new_product = {
+            "id": len(products) + 1,
+            "category": request.form['category'],
+            "name": request.form['name'],
+            "brand": request.form['brand'],
+            "Supplier": request.form['Supplier'],
+            "quantity": int(request.form['quantity']),
+            "minimum": int(request.form['minimum']),
+            "expiration": request.form['expiration']
+        }
+        products.append(new_product)
+        return redirect(url_for('Stock'))
     return render_template('add.html')
 
 if __name__ == "__main__":
